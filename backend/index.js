ae484e0af9d63608c36ca951ea2cc01f1557133f
Rajai Bhavik <rajaibhavik38@gmail.com>
2026-01-18 00:09:23 +0530

    fix server crash

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ app.use(cors({
     origin:["http://localhost:5173","https://learning-management-system-6d3763.netlify.app/"],
     credentials:true
 }))
-app.use(express.static(path.join(__dirname, 'public')));
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
